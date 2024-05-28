99374201f194a9e83a9da46df1835054ba8f821d
nasroos <nasreddine.merdji10.@e-uvt.ro>
2024-05-28 18:44:44 +0300

    first Commit

---
+++ CMakeFiles/clinic_management_system.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/os/CLionProjects/untitled3/Appointment.cpp" "CMakeFiles/clinic_management_system.dir/Appointment.cpp.o" "gcc" "CMakeFiles/clinic_management_system.dir/Appointment.cpp.o.d"
+  "/home/os/CLionProjects/untitled3/Billing.cpp" "CMakeFiles/clinic_management_system.dir/Billing.cpp.o" "gcc" "CMakeFiles/clinic_management_system.dir/Billing.cpp.o.d"
+  "/home/os/CLionProjects/untitled3/ClinicManagementSystem.cpp" "CMakeFiles/clinic_management_system.dir/ClinicManagementSystem.cpp.o" "gcc" "CMakeFiles/clinic_management_system.dir/ClinicManagementSystem.cpp.o.d"
+  "/home/os/CLionProjects/untitled3/Patient.cpp" "CMakeFiles/clinic_management_system.dir/Patient.cpp.o" "gcc" "CMakeFiles/clinic_management_system.dir/Patient.cpp.o.d"
+  "/home/os/CLionProjects/untitled3/main.cpp" "CMakeFiles/clinic_management_system.dir/main.cpp.o" "gcc" "CMakeFiles/clinic_management_system.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/clinic_management_system.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/clinic_management_system.dir/Appointment.cpp.o"
+  "CMakeFiles/clinic_management_system.dir/Appointment.cpp.o.d"
+  "CMakeFiles/clinic_management_system.dir/Billing.cpp.o"
+  "CMakeFiles/clinic_management_system.dir/Billing.cpp.o.d"
+  "CMakeFiles/clinic_management_system.dir/ClinicManagementSystem.cpp.o"
+  "CMakeFiles/clinic_management_system.dir/ClinicManagementSystem.cpp.o.d"
+  "CMakeFiles/clinic_management_system.dir/Patient.cpp.o"
+  "CMakeFiles/clinic_management_system.dir/Patient.cpp.o.d"
+  "CMakeFiles/clinic_management_system.dir/main.cpp.o"
+  "CMakeFiles/clinic_management_system.dir/main.cpp.o.d"
+  "clinic_management_system"
+  "clinic_management_system.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/clinic_management_system.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
